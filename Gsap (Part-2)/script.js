a514b85f77c5258319a9aba22430b06fd44b6b5b
Muhammad Umar Khan <muhammadumarkhan72@gmail.com>
2026-03-15 16:00:15 +0500

    Add project files

--- Gsap (Part-2)/script.js
+++ Gsap (Part-2)/script.js
@@ -25,14 +25,86 @@
 //     }
 // })
 
-gsap.from("#page2 h1", {
-    opacity:0,
-    duration:2,
-    x:500,
+// gsap.from("#page2 h1", {
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger: {
+//         trigger: "#page2 h1",
+//         scroller:"body",
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+// gsap.from("#page2 h2", {
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger: {
+//         trigger: "#page2 h2",
+//         scroller:"body",
+//         markers: true,
+//         start: "top 50%"
+//     }
+// })
+
+// gsap.from("#page2 #box", {
+//     scale: 0,
+//     opacity:0,
+//     rotate:720,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         scrub:true
+//     }
+// })
+
+// gsap.from("#page2 #box", {
+//     scale: 0,
+//     opacity:0,
+//     rotate:720,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         scrub:2
+//     }
+// })
+
+
+// gsap.from("#page2 #box", {
+//     scale: 0,
+//     opacity:0,
+//     rotate:720,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%",
+//         end:"top 30%",
+//         scrub:2,
+//         pin:true
+//     }
+// })
+
+gsap.to("#page2 h1", {
+    transform:"translateX(-150%)",
     scrollTrigger: {
-        trigger: "#page2 h1",
-        scroller:"body",
+        trigger: "#page2",
+        scroller: "body",
         markers: true,
-        start: "top 50%"
+        start: "top 0%",
+        end: "top -150%",
+        scrub:2,
+        pin: true
     }
 })
